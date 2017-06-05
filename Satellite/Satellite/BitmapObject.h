b48271d530259299e0d86df5819983d5d8dbf5ab
JuHee <thdwngml1223@gmail.com>
2017-06-05 19:22:31 +0900

    edit png alpa sprite

--- Satellite/Satellite/BitmapObject.h
+++ Satellite/Satellite/BitmapObject.h
@@ -19,6 +19,28 @@ public:
 		m_bmpsize.x = m_cimg.GetWidth();
 		m_bmpsize.y = m_cimg.GetHeight();
 
+		if (m_cimg.GetBPP() == 32)
+		{
+			unsigned char * pCol = 0;
+			long w = m_bmpsize.x;
+			long h = m_bmpsize.y;
+			for (long y = 0; y < h; y++)
+			{
+				for (long x = 0; x < w; x++)
+				{
+					pCol = (unsigned char *)m_cimg.GetPixelAddress(x, y);
+					unsigned char alpha = pCol[3];
+					if (alpha != 255)
+					{
+						pCol[0] = ((pCol[0] * alpha) + 128) >> 8;
+						pCol[1] = ((pCol[1] * alpha) + 128) >> 8;
+						pCol[2] = ((pCol[2] * alpha) + 128) >> 8;
+					}
+				}
+			}
+			m_cimg.SetHasAlphaChannel(true);
+		}
+
 	}
 	virtual void draw(HDC hdc) override;
 
